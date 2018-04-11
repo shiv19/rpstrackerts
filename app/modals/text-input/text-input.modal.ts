3a410a3a75fafcf6bce60304411b7fd2663e8c46
Alexander Ziskind <alex@nuvious.com>
2018-04-10 22:38:19 -0400

    text input modal generics fix

--- app/modals/text-input/text-input.modal.ts
+++ app/modals/text-input/text-input.modal.ts
@@ -2,7 +2,7 @@ import { ShownModallyData, Page } from 'ui/page';
 
 import { TextInputModalViewModel } from './text-input.modal.vm';
 
-export function onShownModally<T, R>(args: ShownModallyData) {
+export function onShownModally(args: ShownModallyData) {
     const page = <Page>args.object;
     const textInputModal = new TextInputModalViewModel(args);
     page.bindingContext = textInputModal;
