3fbc0079c0a9139faa8afb9ae5578317cc0fe243
Alexander Ziskind <alex@nuvious.com>
2018-05-08 08:59:35 -0400

    added logging contracts

--- app/core/contracts/repositories/index.ts
+++ app/core/contracts/repositories/index.ts
@@ -1,4 +1,5 @@
-export * from './pt-user-repository.contract';
+export * from './pt-auth-repository.contract';
 export * from './pt-backlog-repository.contract';
+export * from './pt-logging-repository.contract';
 export * from './pt-storage-repository.contract';
 export * from './pt-user-repository.contract';
--- app/core/contracts/services/index.ts
+++ app/core/contracts/services/index.ts
@@ -1,6 +1,7 @@
 export * from './pt-auth-service.contract';
 export * from './pt-backlog-service.contract';
 export * from './pt-comment-service.contract';
+export * from './pt-logging-service.contract';
 export * from './pt-storage-service.contract';
 export * from './pt-task-service.contract';
 export * from './pt-user-service.contract';
